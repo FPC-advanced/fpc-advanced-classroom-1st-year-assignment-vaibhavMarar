cf9726ba9f03a34c49e2a2837d273f021207f703
vaibhav marar <vaibhavMarar>
2023-10-30 16:21:36 +0530

    committed 10/30/2023, 4:21:35 PM

---
+++ set01/problem10.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void input_two_strings(char *string1,char *string2)
+{
+    printf("Input the first string: ");
+    scanf("%s",string1);
+    printf("Input the second string: ");
+    scanf("%s",string2);
+}
+
+int stringcompare(char *string1, char *string2)
+{
+    int i=0,flag=0;
+    while(string1[i]!='\0' || string2[i]!='\0')
+    {
+        if (string1[i]>string2[i])
+        {
+            flag=1;
+        }
+        else if (string1[i]<string2[i])
+        {
+            flag=2;
+        }
+        else{
+            flag=0;
+        }
+    i++;
+    }
+    return flag;
+}
+
+void output(char *string1, char *string2, int result)
+{
+    if (result==1)
+    {
+        printf("The greater string is %s",string1);
+    }
+    else if (result==2)
+    {
+       printf("The greater string is %s",string2); 
+    }
+    else
+    {
+        printf("The strings are equal");
+        }
+}
+
+int main()
+{
+    char str1,str2,result;
+    input_two_strings(&str1,&str2);
+    result = stringcompare(&str1,&str2);
+    output(&str1,&str2,result);
+    return 0;
+}
