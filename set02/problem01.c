ed300aff1f825fd1d5d279613ed23440e4235f48
vaibhav marar <vaibhavMarar>
2023-10-31 05:52:54 +0530

    committed 10/31/2023, 5:52:53 AM

---
+++ set02/problem01.c
@@ -0,0 +1,8 @@
+/*01.  Write a program to find the area of a triangle.
+
+***Function Declarations***
+```c
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);  */
+
