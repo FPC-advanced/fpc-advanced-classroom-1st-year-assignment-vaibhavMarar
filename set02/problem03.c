04a944604cf8eee138a97f3b5a3987f9efc8aea7
vaibhav marar <vaibhavMarar>
2023-11-01 08:02:30 +0530

    committed 11/1/2023, 8:02:29 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -14,7 +14,7 @@ int input_number(){
 int is_composite(int n){
    if (n <= 1) {
         printf("Number should be greater than 1!");
-        return 0;
+        return -1;
     }
     for (int i = 2; i < n; i++) {
         if (n % i == 0) {
