261e45c19aacd8ccd1fc9a6d1dc2c01d7e2c07e8
vaibhav marar <vaibhavMarar>
2023-11-01 14:47:12 +0530

    committed 11/1/2023, 2:47:11 PM

---
+++ set02/problem04.c
@@ -0,0 +1,49 @@
+/*
+Write a program to find Sum of composite number in an array of different numbers entered by the user.
+
+***Function Declarations***
+```c
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+*/
+#include <stdio.h>
+int input_array_size(){
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d",&size);
+    return size;
+}
+
+void input_array(int n, int a[n]){
+    printf("enter the size of array: ");
+    for(int i=0; i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+int sum_composite_numbers(int n, int a[n]){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(a[i] >i){
+            for(int j=2;j<=a[i];j++){
+                if(a[i]%2 ==0){
+                    sum+= a[i];
+                    break;
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+void output(int sum){
+    printf("the sum of all composite numbers in the following is %d",sum);
+}
+
+int main(){
+    int n,a[n],sum;
+    n = input_array_size();
+    input_array
+}
+inpuinput_array
