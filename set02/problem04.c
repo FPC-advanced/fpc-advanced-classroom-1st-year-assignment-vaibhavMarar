1b68c17b6e521bbf72bcd8cfb54c91b1527190d8
vaibhav marar <vaibhavMarar>
2023-11-02 05:51:00 +0530

    committed 11/2/2023, 5:50:59 AM

--- set02/problem04.c
+++ set02/problem04.c
@@ -27,7 +27,7 @@ int sum_composite_numbers(int n, int a[n]){
     for(int i=0;i<n;i++){
         if(a[i] >i){
             for(int j=2;j<=a[i];j++){
-                if(a[i]%2 ==0){
+                if(a[i]%j ==0){
                     sum+= a[i];
                     break;
                 }
