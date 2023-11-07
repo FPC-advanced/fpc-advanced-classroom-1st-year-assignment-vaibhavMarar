81ee58873113839dfad6bc579c40b900d752288b
vaibhav marar <vaibhavMarar>
2023-11-07 05:50:42 +0530

    committed 11/7/2023, 5:50:41 AM

---
+++ set02/problem06.c
@@ -0,0 +1,18 @@
+/*
+Write a program to reverse a string.
+
+***Function Declarations***
+```c
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+*/
+#include <stdio.h>
+void input_string(char *a){
+     printf("enter a string");
+     scanf("%d",a);
+}
+void str_reverse(char *str, char *rev_str) {
+    
+}
+ 
