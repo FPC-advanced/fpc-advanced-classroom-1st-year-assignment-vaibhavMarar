c14b7ac9b08cd1b4f09de248cf7f97b44fd65930
vaibhav marar <vaibhavMarar>
2023-11-09 15:42:24 +0530

    committed 11/9/2023, 3:42:23 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -10,11 +10,11 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 #include <stdio.h>
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
     printf("Enter coordinate for point 1: ");
-    scanf("%f %f %f",x1,y1);
+    scanf("%f %f",x1,y1);
     printf("Enter coordinate for point 2: ");
-    scanf("%f %f %f",x2,y2);
+    scanf("%f %f",x2,y2);
     printf("Enter coordinate for point 3: ");
-    scanf("%f %f %f",x3,y3);
+    scanf("%f %f",x3,y3);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
    float result = 0.5 *(x1*(y2-y3) +x2*(y3-y1) + x3*(y1-y2));
