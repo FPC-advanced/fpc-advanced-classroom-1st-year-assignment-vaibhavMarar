d173fe9e7d14fdc1262b86d42c3fa2457fdfd6de
vaibhav marar <vaibhavMarar>
2023-11-09 15:13:53 +0530

    committed 11/9/2023, 3:13:52 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -15,8 +15,17 @@ void input(float *x1, float *y1, float *x2, float *y2){
     printf("enter coordinates of second point: ");
     scanf("%f %f",x2,y2);
 }
+float find_distance(float x1, float y1, float x2, float y2){
+    float distance = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return distance;
+}
+void output(float x1, float y1, float x2, float y2, float distance){
+    printf("the distance between (%.2lf,%.2lf) and (%.2lf,%2lf) is %.2lf",x1,y1,x2,y2,distance);
+}
 int main(){
-    float x1,y1,x2,y2;
+    float x1,y1,x2,y2,distance;
     input(&x1,&y1,&x2,&y2);
+    distance = find_distance(x1,y1,x2,y2);
+    output(x1,y1,x2,y2,distance);
     return 0;
 }
