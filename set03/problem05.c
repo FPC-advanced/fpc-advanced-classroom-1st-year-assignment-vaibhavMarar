73786c65c3e91df173d6fcaaf5c4a948b112fe97
vaibhav marar <vaibhavMarar>
2023-11-12 20:54:34 +0530

    committed 11/12/2023, 8:54:34 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -8,3 +8,48 @@ void init_array(int n, int a[n];
 void erotosthenes_sieve(int n, int a[n]);
 void output(int n, int a[n]);
 */
+#include<stdio.h>
+
+int input_array_size() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    return n;
+}
+
+void init_array(int n, int a[n]) {
+    for (int i = 0; i < n; i++) {
+        a[i] = 1; // Initialize all elements to 1 (assumed prime)
+    }
+    a[0] = a[1] = 0; // 0 and 1 are not prime
+}
+
+void eratosthenes_sieve(int n, int a[n]) {
+    for (int i = 2; i * i <= n; i++) {
+        if (a[i] == 1) {
+            for (int j = i * i; j <= n; j += i) {
+                a[j] = 0; // Mark multiples of p as non-prime
+            }
+        }
+    }
+}
+
+void output(int n, int a[n]) {
+    printf("Prime numbers between 2 and %d are: \n", n);
+    for (int i = 2; i <= n; i++) {
+        if (a[i] == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+int main() {
+    int n = input_array_size();
+    int a[n];
+
+    init_array(n, a);
+    eratosthenes_sieve(n, a);
+    output(n, a);
+
+    return 0;
+}
