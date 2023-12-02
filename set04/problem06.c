46b2b5f5aec35f0c7c2ecdb0cf588d12c1d73f87
vaibhav marar <vaibhavMarar>
2023-12-02 18:27:24 +0530

    committed 12/2/2023, 6:27:24 PM

---
+++ set04/problem06.c
@@ -0,0 +1,47 @@
+/*
+Write a program to count the number of words in a string using strtok (_string.h_)
+
+***Function Declarations***
+```c
+void input_string(char *a);
+int count_words(char *string);
+void output(char *string, int no_words);
+*/
+#include<stdint.h>
+void input_string(char *a) {
+    printf("Enter a string: ");
+    fgets(a, 100, stdin); // Reading up to 100 characters including spaces
+}
+
+// Function to count the number of words in the string
+int count_words(char *string) {
+    int count = 0;
+    char *token = strtok(string, " \t\n"); // Tokenizing based on space, tab, and newline characters
+
+    while (token != NULL) {
+        count++;
+        token = strtok(NULL, " \t\n");
+    }
+
+    return count;
+}
+
+// Function to output the result
+void output(char *string, int no_words) {
+    printf("The string \"%s\" contains %d words.\n", string, no_words);
+}
+int main() {
+    char inputString[100]; // Assuming a maximum input string length of 100 characters
+    int wordCount;
+
+    // Input the string
+    input_string(inputString);
+
+    // Count the number of words
+    wordCount = count_words(inputString);
+
+    // Output the result
+    output(inputString, wordCount);
+
+    return 0;
+}
