173805c9d41ff6835505d686d3d6aebefa25a112
vaibhav marar <vaibhavMarar>
2023-12-02 17:38:09 +0530

    committed 12/2/2023, 5:38:08 PM

--- set04/problem03.c
+++ set04/problem03.c
@@ -8,9 +8,6 @@ int nCr(int n, int r);
 void output(int n, int r, int result);
 */
 #include<stdio.h>
-void input_n_and_r(int *n, int *r);
-int nCr(int n, int r);
-void output(int n, int r, int result);
 
 
 
