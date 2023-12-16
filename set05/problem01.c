9f9f1290856b9aa0f4d39151797a6fa2efc76322
vaibhav marar <vaibhavMarar>
2023-12-16 08:54:10 +0530

    committed 12/16/2023, 8:54:08 AM

---
+++ set05/problem01.c
@@ -0,0 +1,44 @@
+/*
+ Write a program to find the distance between two points.
+
+***Function Declarations***
+
+```c
+struct _point {
+  float x;
+  float y;
+};
+*/
+#include<stdio.h>
+#include<math.h>
+struct _point
+{
+    float x;
+    float y;
+};
+typedef struct _point Point;
+
+
+float calculate_dist(Point p1,Point p2){
+    return sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+}
+
+int main(){
+    Point pnta,pntb;
+    printf("enter the value of x-coordinate of first point: ");
+    scanf("%d",&pnta.x);
+
+    printf("enter the value of y-coordinate of first point: ");
+    scanf("%d",&pnta.y);
+
+    printf("enter the value of x-coordinate of second point: ");
+    scanf("%d",&pntb.x);
+
+    printf("enter the value of y-coordinate of second point: ");
+    scanf("%d",&pntb.y);
+
+    float distance=calculate_dist(pnta,pntb);
+    printf("\nThe distance between (%.2lf,%.2lf) and (%.2lf,%.2lf)is %.2lf",distance);
+    
+    return 0;
+}
