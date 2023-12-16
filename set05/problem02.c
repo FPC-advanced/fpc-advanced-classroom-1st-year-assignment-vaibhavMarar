63078c22c14205ccd130eb40a5f51a727748df96
vaibhav marar <vaibhavMarar>
2023-12-16 10:31:25 +0530

    committed 12/16/2023, 10:31:24 AM

---
+++ set05/problem02.c
@@ -0,0 +1,17 @@
+/*
+Write a program to find the weight of a camel, given height, length and stomach radius using four functions.
+
+> `weight = pi * stomach_radius^3 * sqrt(height * length)`
+
+***Function Declarations***
+
+```c
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length, float weight);
+*/
+#include<stdio.h>
+#include<math.h>
+void input_camel_details(float *radius, float *height, float *length){
+    printf("Enter the value for Stomach Radius: ");
+}__m__mingw_printf
