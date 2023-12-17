ea98a720e20cce6367f210b62946f9863213128d
vaibhav marar <vaibhavMarar>
2023-12-17 08:52:20 +0530

    committed 12/17/2023, 8:52:18 AM

---
+++ set05/problem05.c
@@ -0,0 +1,7 @@
+/*
+The formula for finding borga(x) is 1 + (x^1)/3! + (x^2)/5! + (x^3)/7! + .... Stop when the next term is less 0.000001 using four functions.
+
+int input(int x);
+float borga_X(int x);
+void output(int x, float result);
+*/
